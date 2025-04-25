6a6be332a1f51bec445aab5eb30eebb31e5e2e39
janka <jan.kalisz007@gmail.com>
2025-04-25 13:43:56 +0200

    Betterest linking, 😎

--- layout/assets.php
+++ layout/assets.php
@@ -1,14 +1,14 @@
-<?php $base_url = 'thescruncher'?>
+<?php $base_url = '/thescruncher/'?>
 <title>The Scruncher</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta charset="UTF-8">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="/<?= $base_url?>/layout/css/base.css">
-<link rel="stylesheet" href="/<?= $base_url?>/layout/css/card.css">
-<link rel="stylesheet" href="/<?= $base_url?>/layout/css/form.css">
-<link rel="stylesheet" href="/<?= $base_url?>/layout/css/footer.css">
-<link rel="stylesheet" href="/<?= $base_url?>/layout/css/zoom.css">
-<link rel="stylesheet" href="/<?= $base_url?>/layout/css/navbar.css">
+<link rel="stylesheet" href="<?= $base_url?>layout/css/base.css">
+<link rel="stylesheet" href="<?= $base_url?>layout/css/card.css">
+<link rel="stylesheet" href="<?= $base_url?>layout/css/form.css">
+<link rel="stylesheet" href="<?= $base_url?>layout/css/footer.css">
+<link rel="stylesheet" href="<?= $base_url?>layout/css/zoom.css">
+<link rel="stylesheet" href="<?= $base_url?>layout/css/navbar.css">
 <link rel="icon" href="/icon.png" type="image/png">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-<script src="<?= $base_url?>/layout/js/zoomer.js"></script>
+<script src="<?= $base_url?>layout/js/zoomer.js"></script>
